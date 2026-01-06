c87b89e716637b3b86730cc88c470681bb74c559
Slumpy_666 <dr.slumpy.666@gmail.com>
2026-01-06 18:40:05 -0500

    fixed ordering of lots

--- backend/parking_system/parking/views.py
+++ backend/parking_system/parking/views.py
@@ -17,16 +17,16 @@ from .serializers import (
 
 
 class ParkingLotViewSet(viewsets.ModelViewSet):
-    queryset = ParkingLot.objects.all()
+    queryset = ParkingLot.objects.all().order_by('parking_lot_id')
     serializer_class = ParkingLotSerializer
 
 
 class ParkingSpotViewSet(viewsets.ModelViewSet):
-    queryset = ParkingSpot.objects.all()
+    queryset = ParkingSpot.objects.all().order_by('spot_id')
     serializer_class = ParkingSpotSerializer
 
     def get_queryset(self):
-        queryset = ParkingSpot.objects.all()
+        queryset = ParkingSpot.objects.all().order_by('spot_id')
         lot_id = self.request.query_params.get('parking_lot')
         if lot_id:
             queryset = queryset.filter(parking_lot_id=lot_id)
@@ -62,7 +62,7 @@ class VehicleViewSet(viewsets.ModelViewSet):
 @api_view(['GET'])
 def dashboard_summary(request):
     """Quick summary endpoint for the frontend dashboard."""
-    lots = ParkingLot.objects.all()
+    lots = ParkingLot.objects.all().order_by('parking_lot_id')
     data = []
     for lot in lots:
         total = lot.spots.count()
@@ -99,13 +99,13 @@ def lots_for_permit(request):
     """Get parking lots accessible for the current user's permit type."""
     if not request.user.is_authenticated:
         # Return all lots for anonymous users
-        lots = ParkingLot.objects.all()
+        lots = ParkingLot.objects.all().order_by('parking_lot_id')
     elif request.user.permit_type:
         # Return lots that allow this permit type
-        lots = ParkingLot.objects.filter(permit_types=request.user.permit_type)
+        lots = ParkingLot.objects.filter(permit_types=request.user.permit_type).order_by('parking_lot_id')
     else:
         # User has no permit, return empty or all lots
-        lots = ParkingLot.objects.all()
+        lots = ParkingLot.objects.all().order_by('parking_lot_id')
     
     serializer = ParkingLotSerializer(lots, many=True)
     return Response(serializer.data)
